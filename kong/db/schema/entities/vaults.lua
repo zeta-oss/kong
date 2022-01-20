74c8ebc5ab6bc9e167228896f715ddd9f95c3a29
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2022-01-20 11:35:34 +0200

    feat(db) vaults entity schema

---
+++ kong/db/schema/entities/vaults.lua
@@ -0,0 +1,22 @@
+local typedefs = require "kong.db.schema.typedefs"
+
+
+return {
+  name = "vaults",
+  primary_key = { "id" },
+  cache_key = { "prefix" },
+  endpoint_key = "prefix",
+  workspaceable = true,
+  subschema_key = "name",
+  subschema_error = "vault '%s' is not installed",
+  fields = {
+    { id = typedefs.uuid },
+    -- note: prefix must be valid in a host part of vault reference uri:
+    -- {vault://<vault-prefix>/<secret-id>[/<secret-key]}
+    { prefix = { type = "string", required = true, unique = true, unique_across_ws = true, match = [[^[a-z][a-z%d-]-[a-z%d]+$]] }},
+    { name = { type = "string", required = true }},
+    { description = { type = "string" }},
+    { config = { type = "record", abstract = true }},
+    { tags = typedefs.tags },
+  },
+}
